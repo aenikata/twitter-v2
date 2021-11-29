091be3e12cec18dcd9093413576ea9661107e968
David Burton <david@grid5.co.uk>
2021-11-29 01:25:32 +0000

    Lint and version update

--- test/e2e/search.js
+++ test/e2e/search.js
@@ -13,24 +13,25 @@ if (!process.env.TWITTER_DISABLE_E2E) {
       );
 
       // Recent Tweet Search API Reference: https://bit.ly/3jqFjKF
-      const { data: tweets, meta, errors } = await client.get(
-        'tweets/search/recent',
-        {
-          query: 'url:"https://medium.com" -is:retweet lang:en',
-          max_results: 10,
-          tweet: {
-            fields: [
-              'created_at',
-              'entities',
-              'in_reply_to_user_id',
-              'public_metrics',
-              'referenced_tweets',
-              'source',
-              'author_id',
-            ],
-          },
-        }
-      );
+      const {
+        data: tweets,
+        meta,
+        errors,
+      } = await client.get('tweets/search/recent', {
+        query: 'url:"https://medium.com" -is:retweet lang:en',
+        max_results: 10,
+        tweet: {
+          fields: [
+            'created_at',
+            'entities',
+            'in_reply_to_user_id',
+            'public_metrics',
+            'referenced_tweets',
+            'source',
+            'author_id',
+          ],
+        },
+      });
 
       expect(errors).to.be.undefined;
       expect(tweets).to.not.be.empty;
@@ -49,24 +50,25 @@ if (!process.env.TWITTER_DISABLE_E2E) {
       );
 
       // Recent Tweet Search API Reference: https://bit.ly/3jqFjKF
-      const { data: tweets, meta, errors } = await client.get(
-        'tweets/search/recent',
-        {
-          query: 'url:"https://medium.com" -is:retweet lang:en',
-          max_results: 10,
-          tweet: {
-            fields: [
-              'created_at',
-              'entities',
-              'in_reply_to_user_id',
-              'public_metrics',
-              'referenced_tweets',
-              'source',
-              'author_id',
-            ],
-          },
-        }
-      );
+      const {
+        data: tweets,
+        meta,
+        errors,
+      } = await client.get('tweets/search/recent', {
+        query: 'url:"https://medium.com" -is:retweet lang:en',
+        max_results: 10,
+        tweet: {
+          fields: [
+            'created_at',
+            'entities',
+            'in_reply_to_user_id',
+            'public_metrics',
+            'referenced_tweets',
+            'source',
+            'author_id',
+          ],
+        },
+      });
 
       expect(errors).to.be.undefined;
       expect(tweets).to.not.be.empty;
